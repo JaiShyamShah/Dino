e0700add0e6b13c406d722feba4d7a2fbcf7c880
Jai Shah <shah.jai2808@gmail.com>
2026-05-16 17:31:29 +0530

    Added initial error handling

--- kilo.c
+++ kilo.c
@@ -1,27 +1,61 @@
+/* includes */
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
-struct termios orig_termios;
+/* data */
+ struct termios orig_termios;
+
+/* terminal */
+void die(const char *s){
+    perror(s);
+    exit(1);
+}
 
 void disableRawMode(){
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
-}
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+ }
 
 void enableRawMode() {
     // Get the original terminal so we can return that to user on exit
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcsetattr");
     atexit(disableRawMode);
     // Enter raw mode since default is cannonical
-    struct termios raw;
-    tcgetattr(STDIN_FILENO, &raw);
-    raw.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-}
+    struct termios raw = orig_termios;
+    // Miscellaneous flags BRKINT, INPCK, ISTRIP, and CS8
+    // Disable software flow control, carriage return
+    raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+    // Disable output processing
+    raw.c_oflag &= ~(OPOST);
+    // Disable canonical, interupt signals( SIGINT AND SIGTSTP), wait
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cflag &= ~(CS8);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
-int main(){
-    enableRawMode();
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c!='q');
-	return 0;
-}
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsettr");
+ }
+
+/* init */
+ int main(){
+     enableRawMode();
+     while (1) {
+         char c = '\0';
+         if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+             die("read");
+         if (iscntrl(c)) {
+             printf("%d\r\n", c);
+         } else {
+             printf("%d ('%c')\r\n", c, c);
+         }
+         if ((c == 'q') | (c == 'Q')) break;
+     }
+     return 0;
+ }
+
