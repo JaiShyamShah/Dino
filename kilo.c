7249fa7c9a10ea95c59e42583a2a30113ba5f2e5
Jai Shah <shah.jai2808@gmail.com>
2026-05-17 22:58:55 +0530

    Implemented raw input and output changes invlolving window size for tilda, ctl q for exit and keyboard input refactoring

--- kilo.c
+++ kilo.c
@@ -5,28 +5,38 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+/* defines */
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 /* data */
- struct termios orig_termios;
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
 
+struct editorConfig E;
 /* terminal */
-void die(const char *s){
+void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
 
-void disableRawMode(){
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
  }
 
 void enableRawMode() {
     // Get the original terminal so we can return that to user on exit
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcsetattr");
     atexit(disableRawMode);
     // Enter raw mode since default is cannonical
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     // Miscellaneous flags BRKINT, INPCK, ISTRIP, and CS8
     // Disable software flow control, carriage return
     raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
@@ -42,20 +52,65 @@ void enableRawMode() {
         die("tcsettr");
  }
 
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+int getWindowSize(int * rows, int * cols) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/* output */
+void editorDrawRows(){
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~\r\n",3);
+    }
+}
+void editorRefreshScreen(){
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/* input */
+void editorProcessKeypress() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
 /* init */
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
  int main(){
      enableRawMode();
+     initEditor();
      while (1) {
-         char c = '\0';
-         if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
-             die("read");
-         if (iscntrl(c)) {
-             printf("%d\r\n", c);
-         } else {
-             printf("%d ('%c')\r\n", c, c);
-         }
-         if ((c == 'q') | (c == 'Q')) break;
+         editorRefreshScreen();
+         editorProcessKeypress();
      }
      return 0;
  }
-
